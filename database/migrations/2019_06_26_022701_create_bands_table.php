2505c59379fdcc9a40461f17dc4f424d64c0449e
Jacob Sewell <jasewell@gmail.com>
2019-06-26 21:00:42 -0500

    Create migrations for bands and albums tables.

---
+++ database/migrations/2019_06_26_022701_create_bands_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBandsTable extends Migration
+{
+    /**
+     * Create bands table.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        try {
+            Schema::create('bands', function (Blueprint $table) {
+                $table->bigIncrements('band_id');
+
+                $table->string('name', 255);
+
+                $table->date('start_date')->nullable();
+
+                $table->string('website', 255)->nullable();
+
+                $table->boolean('still_active');
+
+                $table->timestamps();
+            });
+        } catch (Throwable $e) {
+            // Having to manually drop a table when your migration fails is annoying.
+            if (Schema::hasTable('bands')) Schema::drop('bands');
+            throw $e;
+        }
+    }
+
+    /**
+     * Drop bands table.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('bands');
+    }
+}
---
+++ database/migrations/2019_06_26_023511_create_albums_table.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAlbumsTable extends Migration
+{
+    /**
+     * Create albums table
+     *
+     * @return void
+     */
+    public function up()
+    {
+        try {
+            Schema::create('albums', function (Blueprint $table) {
+                $table->bigIncrements('album_id');
+
+                $table->unsignedBigInteger('band_id');
+                $table->foreign('band_id')
+                    ->references('band_id')
+                    ->on('bands')
+                    ->onDelete('cascade');
+
+                $table->string('name', 255);
+
+                $table->date('recorded_date')->nullable();
+
+                $table->date('release_date')->nullable();
+
+                $table->unsignedSmallInteger('number_of_tracks')->nullable();
+
+                $table->string('label', 255);
+
+                $table->string('producer', 255)->nullable();
+
+                $table->string('genre', 255);
+
+                $table->timestamps();
+            });
+        } catch (Throwable $e) {
+            // Having to manually drop a table when your migration fails is annoying.
+            if (Schema::hasTable('albums')) Schema::drop('albums');
+            throw $e;
+        }
+    }
+
+    /**
+     * Drop albums table
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('albums');
+    }
+}
